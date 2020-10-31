ee78da34b60ffd2c6b891cb6d69deed641a495ff
Dotcom-MK <dotcom070895@gmail.com>
2020-10-31 13:55:35 +0700

    Recheck create form int create file

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
     <title>Todos</title>
 </head>
 <body>
     <div>
         <h1>What next you need To-Do</h1>
-        <form​​ method="POST" action="/todos/create">
+        <form action="/todos/create" method="post">
             @csrf
             <input type="text" name="title">
             <input type="submit" value="Create">
